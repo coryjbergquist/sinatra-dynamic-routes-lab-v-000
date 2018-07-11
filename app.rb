2332b35b3da9e9b22a4aab880b93a59a51df0dca
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-07-11 20:47:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ class App < Sinatra::Base
   end
 
   get "/:operation/:number1/:number2" do
-    "#{:number1} #{:operation} #{:number2}"
+    "#{:number1.to_i} #{:operation} #{:number2.to_i}"
   end
 
 end
